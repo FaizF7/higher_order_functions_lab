a214e8018325825df4b5144ebcf74e7e4ef74cd6
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-22 17:19:28 +0000

    MVP completed

--- ScranAdvisor.js
+++ ScranAdvisor.js
@@ -3,5 +3,29 @@ const ScranAdvisor = function(restaurants){
 }
 
 // Add methods to prototype
+ScranAdvisor.prototype.countRestaurants = function(){
+    return this.restaurants.length
+}
+
+ScranAdvisor.prototype.findByName = function(name){
+    return this.restaurants.find((restaurant)=>{return restaurant.name===name})
+}
+
+ScranAdvisor.prototype.findAllNames = function(){
+    return this.restaurants.map((restaurant)=>{return restaurant.name})
+}
+
+ScranAdvisor.prototype.findRestaurantByTown = function(townName){
+    return this.restaurants.filter((restaurant)=>{return restaurant.location.town===townName})
+}
+
+ScranAdvisor.prototype.findMostCommonCuisine=function(){
+    console.log(this.restaurants.map((restaurant)=> {restaurant.cuisine}))
+}
+
+const restaurants = require('./restaurants.json')
+scranAdvisor = new ScranAdvisor(restaurants);
+
+scranAdvisor.findMostCommonCuisine()
 
 module.exports = ScranAdvisor;
